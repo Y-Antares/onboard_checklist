1d4ea07562d64fcdce0776585b9c92738e2a9ee8
Y-Antares <2570984488@qq.com>
2023-10-21 10:52:11 +0800

    DOC

---
+++ people/yanzhiyang.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 21 10:29:30 2023
+
+@author: pc
+"""
+import this
+import numpy as np
+import pandas as pd
+np.random.seed(1234)
+d1 = pd.Series(2*np.random.normal(size = 100)+3)
+d2 = np.random.f(2,4,size = 100)
+d3 = np.random.randint(1,100,size = 100)
+
+def status(x) : 
+    return pd.Series([x.count(),x.min(),x.idxmin(),x.quantile(.25),x.median(),
+                      x.quantile(.75),x.mean(),x.max(),x.idxmax(),x.mad(),x.var(),
+                      x.std(),x.skew(),x.kurt()],index=['总数','最小值','最小值位置','25%分位数',
+                    '中位数','75%分位数','均值','最大值','最大值位数','平均绝对偏差','方差','标准差','偏度','峰度'])
+                                                        
+df = pd.DataFrame(status(d1))
+print(df)   
+
+                                                     
